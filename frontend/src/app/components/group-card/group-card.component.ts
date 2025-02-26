21e749e08d7a52157cced4a68465df227f45c17d
csizmadia barnabas <cb20040621@gmail.com>
2025-02-26 16:20:16 +0100

    finished static group cards

---
+++ frontend/src/app/components/group-card/group-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-card',
+  templateUrl: './group-card.component.html',
+  styleUrls: ['./group-card.component.scss'],
+})
+export class GroupCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- frontend/src/app/components/pages/browser/browser.component.ts
+++ frontend/src/app/components/pages/browser/browser.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GroupCardComponent } from '../../group-card/group-card.component';
+
 @Component({
   selector: 'app-browser',
   templateUrl: './browser.component.html',
   styleUrls: ['./browser.component.scss'],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GroupCardComponent],
 })
 export class BrowserComponent implements OnInit {
   ngOnInit() {}
