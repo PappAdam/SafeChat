2586091534a41c6e3111519a240b518b511d7277
Csizmadia Barnabás <csizmadia.barnabas@students.jedlik.eu>
2025-02-27 08:43:07 +0100

    made group-chat component responsive

--- frontend/src/app/components/group-card/group-card.component.ts
+++ frontend/src/app/components/group-card/group-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-group-card',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupCardComponent implements OnInit {
   constructor() {}
+  @Input() unread: string = '0';
+  @Input({ required: true }) name: string = '';
+  @Input({ required: true }) last_msg: string = '';
+  @Input() status: string = '';
 
   ngOnInit() {}
 }
